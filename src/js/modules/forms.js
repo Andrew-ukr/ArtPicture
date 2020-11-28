ffd9fabd0dc0fe142636f6c3262b371212ec581c
Andrii <pashulyaand@gmail.com>
2020-11-28 14:49:35 +0200

    start doing sending form

---
+++ src/js/modules/forms.js
@@ -0,0 +1,50 @@
+const forms = () => {
+  let form = document.querySelectorAll('form');
+  let input = document.querySelectorAll('input[name="phone"]');
+
+  input.forEach(item => {
+    item.addEventListener('input', () => {
+      item.value = item.value.replace(/\D/, '');
+    });
+  });
+
+  let massage = {
+    sending: `Надсилання даних ...`,
+    successfully: `Дані надіслані успішно`,
+    unsuccessful: `Сталася помилка, спробуте ще раз`,
+  };
+
+  form.forEach(item => {
+    item.addEventListener('submit', (e) => {
+      e.preventDefault();
+
+      let statusMassage = document.createElement('div');
+      statusMassage.textContent = massage.sending;
+      item.append(statusMassage);
+
+      function closeStatusMessage() {
+        setTimeout(() => {
+          statusMassage.remove();
+        }, 5000);
+      }
+
+      const formData = new FormData(item);
+
+      fetch('./assets/server.php', {
+          method: "POST",
+          body: formData,
+        }).then(data => data.text())
+        .then(data => {
+          console.log(data);
+          statusMassage.textContent = massage.successfully;
+        }).catch(() => {
+          statusMassage.textContent = massage.unsuccessful;
+        }).finally(() => {
+          closeStatusMessage();
+          item.reset();
+        });
+
+    });
+  });
+};
+export default forms;
